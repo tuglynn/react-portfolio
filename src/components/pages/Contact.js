f0cefde4812b0c097a27251d5f7dc4a32df0b109
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-09-27 22:02:24 -0700

    removed redundant contact labels

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -14,10 +14,7 @@ export default function Contact() {
         <h2>
         Contact Me
     </h2>
-    <a href="https://github.com/monacoglynn" target="_blank" rel="noopener noreferrer">GitHub</a>
     <a href="mailto:pmtgcode@gmail.com?" subject="Questions about code">Email</a>
-    <a href="http://www.linkedin.com/in/patrick-tu-glynn
-    " target="_blank" rel="noopener noreferrer">Linked-In</a>
     </div>
     <div className="resume">
         <h3>Resume</h3>
